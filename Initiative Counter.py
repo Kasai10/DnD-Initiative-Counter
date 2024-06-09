63f4a1fa41416ac49b98e312e2cfdd601415edb2
simo <simobeneckert@gmail.com>
2024-06-09 21:33:17 +0200

    Made some quality of life improvements

--- Initiative Counter.py
+++ Initiative Counter.py
@@ -9,17 +9,27 @@ init()
 def get_characters():
     characters = {}
     while True:
-        character = input("Input a Player (n to continue): ")
-        if(character == "n"):
+        character = input("Input a Player (n to continue): ").capitalize()
+        if(character[-1].isdigit()):
+            print("Input a character without digits!")
+            continue
+        if(character == "N"):
             break
-        initiative = int(input("Initiative Count: "))
-        characters[character] = initiative
+        initiative = input("Initiative Count: ")
+        if not initiative.isdigit():
+            print("Input an Integer!")
+            continue
+        characters[character] = int(initiative)
 
     while True:
-        monster = input("Input a Monster (n to finish): ")
-        if(monster == "n"):
+        monster = input("Input a Monster (n to finish): ").capitalize()
+        if(monster == "N"):
             break
-        number_of_monsters = int(input("Number of Monsters of type: "))
+        number_of_monsters = input("Number of Monsters of type: ")
+        if not number_of_monsters.isdigit():
+            print("Input an integer!")
+            continue
+        number_of_monsters = int(number_of_monsters)
         for i in range(number_of_monsters):
             initiative = random.randint(1, 20)
             characters[monster + " " + str(i + 1)] = initiative
@@ -28,7 +38,15 @@ def get_characters():
 
 def sort_initiative(characters):
     sorted_dict_desc = dict(sorted(characters.items(), key=lambda item: item[1], reverse=True))
-    display_inititative(sorted_dict_desc)
+    counter = 1
+    rename_monsters = {}
+    for key in sorted_dict_desc.keys():
+        if(key[-1].isdigit()):
+            rename_monsters[key[:-1] + str(counter)] = sorted_dict_desc[key]
+            counter = counter + 1
+        else:
+            rename_monsters[key] = sorted_dict_desc[key]
+    display_inititative(rename_monsters)
 
 def display_inititative(characters):
     values = characters.keys()
@@ -38,8 +56,8 @@ def display_inititative(characters):
     # For Linux, macOS, and other Unix-like systems
     else:
         os.system('clear')
-        
-    print("Press d to kill a participant!")
+
+    print("Press d to kill a participant and e to end the programm!")
     print()
     print("Initiative Order: ")
     
@@ -62,6 +80,13 @@ def display_inititative(characters):
                 if(user_input == "d"):
                     color = Fore.RED
                     characters[_] = "d"
+                elif(user_input == "e"):
+                    if os.name == 'nt':
+                        os.system('cls')
+                    # For Linux, macOS, and other Unix-like systems
+                    else:
+                        os.system('clear')
+                    quit()
                 print('\x1b[1A', end='', flush=True)  # Move cursor up
                 print('\x1b[2K', end='', flush=True)  # Clear the line
                 print(color + "\r" + str(_) + Style.RESET_ALL, flush=True, end="") 
